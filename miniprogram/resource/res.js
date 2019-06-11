59d392f7168bdd1e2f16e9b57bf12c33f5898d99
hongcheng.guo <hongcheng.guo@transsion.com>
2019-06-11 17:58:01 +0800

    [小程序]导入题库

--- miniprogram/resource/res.js
+++ miniprogram/resource/res.js
@@ -1,61 +1,75 @@
-let questions = [
-{"answer": "B", "stem": "YICMG全称是什么？（多选）", "options": [{"content": "Youth Innovation Competition on Lancang—Mekong Region's Governance and Development", "option": "A"}, {"content": "Youth Innovation Competition on Global Governance", "option": "B"}, {"content": "澜沧江—湄公河流域治理与发展青年创新设计大赛", "option": "C"}, {"content": "全球治理创新设计大赛", "option": "D"}], "img": null, "no": 1}
-, {"answer": "B", "stem": "澜沧江-湄公河流域一共有几个国家？（单选）", "options": [{"content": "4个", "option": "A"}, {"content": "5个", "option": "B"}, {"content": "6个", "option": "C"}, {"content": "7个", "option": "D"}], "img": null, "no": 2}
-, {"answer": "B", "stem": "举办大赛的目的有哪些？（单选）", "options": [{"content": "鼓励六国青年关注澜湄流域治理和发展的现实问题，深度开展田野调研、社会实践和课外研究。", "option": "A"}, {"content": "通过团队协作提出解决问题、促进发展的有效方案，通过国际竞赛提高学习效果和方案质量，并在有条件视为情况下把方案落实为创新创业活动。", "option": "B"}, {"content": "鼓励六国高等教育和学术研究机构支持广大青年的课外研究和创业行动，组成工作网络，为繁荣区域文化交流、促进国际合作、推动流域治理模式创新提供有力支撑。", "option": "C"}, {"content": "鼓励六国社会力量共同设立澜湄流域治理与发展青年创新创业基金，支持区域合作与青年发展。", "option": "D"}, {"content": "以上都是", "option": "E"}], "img": "https://gss0.bdstatic.com/-4o3dSag_xI4khGkpoWK1HF6hhy/baike/c0%3Dbaike92%2C5%2C5%2C92%2C30/sign=fb529c17221f95cab2f89ae4a87e145b/b999a9014c086e0671cda62205087bf40ad1cb76.jpg", "no": 3}
-, {"answer": "A", "stem": "我国的领陆是否包括海南岛?", "options": [{"content": "包括", "option": "A"}, {"content": "不包括", "option": "B"}], "img": "../../resource/avatar.png", "no": 4}
-, {"answer": "B", "stem": "柴达木盆地是我国的四大盆地之一，其位于我国的哪个高原?", "options": [{"content": "云贵高原", "option": "A"}, {"content": "青藏高原", "option": "B"}], "img": null, "no": 5}
-, {"answer": "A", "stem": "我国于 1958 年 9 月 4 日发表关于领海的声明，宣布中国的领海宽度为____。", "options": [{"content": "12 海里", "option": "A"}, {"content": "200 海里", "option": "B"}], "img": null, "no": 6}
-, {"answer": "A", "stem": "在我国的行政区划中，哪种行政建置稳定地延续了两千多年?", "options": [{"content": "县", "option": "A"}, {"content": "省", "option": "B"}], "img": null, "no": 7}
-, {"answer": "B", "stem": "苏木是内蒙古自治区的一种行政区划单位，其相当于____。", "options": [{"content": "县", "option": "A"}, {"content": "乡", "option": "B"}], "img": null, "no": 8}
-, {"answer": "A", "stem": "我国的邻国中，哪两个国家既是我国的陆上邻国，也是海上邻国?", "options": [{"content": "越南和朝鲜", "option": "A"}, {"content": "老挝和韩国", "option": "B"}], "img": null, "no": 9}
-, {"answer": "B", "stem": "洞朗地区位于西藏自治区日喀则市的____。", "options": [{"content": "聂拉木县", "option": "A"}, {"content": "亚东县", "option": "B"}], "img": null, "no": 11}
-, {"answer": "A", "stem": "元朝时，中央政府设置什么机构管辖澎湖和台湾?", "options": [{"content": "巡检司", "option": "A"}, {"content": "市舶司", "option": "B"}], "img": null, "no": 12}
-, {"answer": "B", "stem": "汉代的文献一般称我国的南海为____。", "options": [{"content": "万里石塘", "option": "A"}, {"content": "涨海", "option": "B"}], "img": null, "no": 13}
-, {"answer": "A", "stem": "《地图管理条例》规定，在地图上绘制中华人民共和国国界，应____。", "options": [{"content": "按照中国国界线画法标准样图绘制", "option": "A"}, {"content": "按照世界各国国界线画法参考样图绘制", "option": "B"}], "img": null, "no": 14}
-, {"answer": "A", "stem": "中国全图是否必须表示南海诸岛?", "options": [{"content": "必须", "option": "A"}, {"content": "不必", "option": "B"}], "img": null, "no": 15}
-, {"answer": "B", "stem": "根据《公开地图内容表示补充规定(试行)》，绘制中国地图(含示意性中国地图)， 中国地图的图形范围上是否可以进行压盖?", "options": [{"content": "可以", "option": "A"}, {"content": "不可以", "option": "B"}], "img": null, "no": 16}
-, {"answer": "A", "stem": "2008 年黑瞎子岛问题的解决，标志着____的边界线全部确定。", "options": [{"content": "中俄", "option": "A"}, {"content": "中越", "option": "B"}], "img": null, "no": 17}
-, {"answer": "B", "stem": "在地图上，西沙群岛可否被标注为“帕拉塞尔群岛”?", "options": [{"content": "可以", "option": "A"}, {"content": "不可以", "option": "B"}], "img": null, "no": 18}
-, {"answer": "B", "stem": "“包公故里”是指下列哪座城市?", "options": [{"content": "开封市", "option": "A"}, {"content": "合肥市", "option": "B"}], "img": null, "no": 19}
-, {"answer": "A", "stem": "台湾省地图是否必须绘出钓鱼岛和赤尾屿?", "options": [{"content": "必须", "option": "A"}, {"content": "不必", "option": "B"}], "img": null, "no": 20}
-, {"answer": "B", "stem": "根据《地图管理条例》，出版地图的，由____送审。", "options": [{"content": "作者", "option": "A"}, {"content": "出版单位", "option": "B"}], "img": null, "no": 21}
-, {"answer": "A", "stem": "2015 年国务院通过《中华人民共和国澳门特别行政区行政区域图(草案)》，该草案划 定了澳门的____。", "options": [{"content": "管理水域", "option": "A"}, {"content": "管理空域", "option": "B"}], "img": null, "no": 22}
-, {"answer": "B", "stem": "有关我国江河湖泊之最的表述，下列关系正确的一组是____。", "options": [{"content": "最大的淡水湖——太湖", "option": "A"}, {"content": "最大的咸水湖——青海湖", "option": "B"}], "img": null, "no": 23}
-, {"answer": "B", "stem": "目前所通行的第五套人民币 100 元纸币的背面图案是____。", "options": [{"content": "泰山", "option": "A"}, {"content": "人民大会堂", "option": "B"}], "img": null, "no": 24}
-, {"answer": "A", "stem": "下列两个地名，读音正确的是____。", "options": [{"content": "乐(lào)亭县", "option": "A"}, {"content": "荥(yíng)阳县", "option": "B"}], "img": null, "no": 25}
-, {"answer": "B", "stem": "下列哪座城市有“成语之乡”的称誉?", "options": [{"content": "淄博市", "option": "A"}, {"content": "邯郸市", "option": "B"}], "img": null, "no": 26}
-, {"answer": "B", "stem": "我国跨纬度最广的省级行政区是____。", "options": [{"content": "新疆维吾尔自治区", "option": "A"}, {"content": "海南省", "option": "B"}], "img": null, "no": 27}
-, {"answer": "A", "stem": "有“黄河源头姊妹湖”之称的是哪两个湖泊?", "options": [{"content": "扎陵湖和鄂陵湖", "option": "A"}, {"content": "可可西里湖和饮马湖", "option": "B"}], "img": null, "no": 28}
-, {"answer": "B", "stem": "清康熙六年(1667 年)将江南省分置成哪两个省?", "options": [{"content": "江苏省和浙江省", "option": "A"}, {"content": "江苏省和安徽省", "option": "B"}], "img": null, "no": 29}
-, {"answer": "B", "stem": "唐古拉山是哪两个省级行政区的重要地理分界线?", "options": [{"content": "新疆维吾尔自治区和西藏自治区", "option": "A"}, {"content": "青海省和西藏自治区", "option": "B"}], "img": null, "no": 30}
-, {"answer": "B", "stem": "红军长征途中刘伯承将军与彝族首领果基小叶丹歃血结盟的故事发生在四川省的哪个 县?", "options": [{"content": "泸定县", "option": "A"}, {"content": "冕宁县", "option": "B"}], "img": null, "no": 31}
-, {"answer": "B", "stem": "我国的梅雨现象一般发生在下列的哪个地区?", "options": [{"content": "华北平原", "option": "A"}, {"content": "长江中下游地区", "option": "B"}], "img": null, "no": 32}
-, {"answer": "A", "stem": "下列哪座山脉是我国第一、二级地形阶梯的分界线?", "options": [{"content": "祁连山脉", "option": "A"}, {"content": "太行山脉", "option": "B"}], "img": null, "no": 33}
-, {"answer": "A", "stem": "塑造华北平原的主力是下列的哪条河流?", "options": [{"content": "黄河", "option": "A"}, {"content": "长江", "option": "B"}], "img": null, "no": 34}
-, {"answer": "B", "stem": "我国四大盆地中，海拔最高的是____。", "options": [{"content": "塔里木盆地", "option": "A"}, {"content": "柴达木盆地", "option": "B"}], "img": null, "no": 35}
-, {"answer": "A", "stem": "《长江之歌》的开头为“你从雪山走来”，歌词中的“雪山”是指____。", "options": [{"content": "唐古拉山", "option": "A"}, {"content": "念青唐古拉山", "option": "B"}], "img": null, "no": 36}
-, {"answer": "B", "stem": "唐代诗人胡曾有诗曰:“千里长河一旦开，亡隋波浪九天来。”诗句中的“千里长河” 是指____。", "options": [{"content": "黄河", "option": "A"}, {"content": "大运河", "option": "B"}], "img": null, "no": 37}
-, {"answer": "A", "stem": "历史上北宋的都城——东京即是今天的____。", "options": [{"content": "开封", "option": "A"}, {"content": "洛阳", "option": "B"}], "img": null, "no": 38}
-, {"answer": "B", "stem": "“雪山连绵，冰川广布”应该描述的是下列哪个地区?", "options": [{"content": "北方地区", "option": "A"}, {"content": "青藏地区", "option": "B"}], "img": null, "no": 39}
-, {"answer": "B", "stem": "当长江发生特大洪水时，下列哪座城市受威胁最大?", "options": [{"content": "杭州", "option": "A"}, {"content": "武汉", "option": "B"}], "img": null, "no": 40}
-, {"answer": "A", "stem": "下列两个省级行政区，人口密度较小的是____。", "options": [{"content": "西藏自治区", "option": "A"}, {"content": "甘肃省", "option": "B"}], "img": null, "no": 41}
-, {"answer": "B", "stem": "成语“泾渭分明”与以下哪个盆地有关?", "options": [{"content": "南阳盆地", "option": "A"}, {"content": "关中盆地", "option": "B"}], "img": null, "no": 42}
-, {"answer": "A", "stem": "丁玲小说《太阳照在桑干河上》中的“桑干河”属于什么水系?", "options": [{"content": "海河水系", "option": "A"}, {"content": "黄河水系", "option": "B"}], "img": null, "no": 43}
-, {"answer": "A", "stem": "雄安新区位于河北省保定市境内，包括雄县、安新、____等 3 县及周边部分区域。", "options": [{"content": "容城", "option": "A"}, {"content": "固安", "option": "B"}], "img": null, "no": 44}
-, {"answer": "B", "stem": "雪顿节是哪个民族的重要节日?", "options": [{"content": "裕固族", "option": "A"}, {"content": "藏族", "option": "B"}], "img": null, "no": 45}
-, {"answer": "A", "stem": "在下列哪座城市可以参观淮海战役纪念馆?", "options": [{"content": "徐州市", "option": "A"}, {"content": "临沂市", "option": "B"}], "img": null, "no": 46}
-, {"answer": "A", "stem": "“复兴号”动车组列车最先在哪条铁路线上运营?", "options": [{"content": "京沪高速铁路", "option": "A"}, {"content": "京广高速铁路", "option": "B"}], "img": null, "no": 47}
-, {"answer": "B", "stem": "下列铁路干线，与长江干流不相交的是____。", "options": [{"content": "京沪高速铁路", "option": "A"}, {"content": "沪昆高速铁路", "option": "B"}], "img": null, "no": 48}
-, {"answer": "B", "stem": "下列哪座城市被中国气象学会授予“中国凉都”的称号?", "options": [{"content": "贵阳市", "option": "A"}, {"content": "六盘水市", "option": "B"}], "img": null, "no": 49}
-, {"answer": "A", "stem": "浙江省的____创办了国内的首个地理信息小镇。", "options": [{"content": "德清县", "option": "A"}, {"content": "桐乡市", "option": "B"}], "img": null, "no": 50}
-, {"answer": "B", "stem": "中国人民解放军建军 90 周年阅兵在朱日和训练基地举行，该基地位于____。", "options": [{"content": "新疆维吾尔自治区", "option": "A"}, {"content": "内蒙古自治区", "option": "B"}], "img": null, "no": 51}
-, {"answer": "A", "stem": "地理歌谣“东方旭日初生，西陲星斗满天”说的是我国疆域____。", "options": [{"content": "东西跨度大", "option": "A"}, {"content": "南北跨度大", "option": "B"}], "img": null, "no": 52}
-, {"answer": "B", "stem": "“大杂小聚居”描述的是我国的____分布特征。", "options": [{"content": "人口", "option": "A"}, {"content": "民族", "option": "B"}], "img": null, "no": 53}
-, {"answer": "B", "stem": "塞罕坝精神植根于塞罕坝的万亩的土地，塞罕坝位于河北省的哪个县境内?", "options": [{"content": "丰宁满族自治县", "option": "A"}, {"content": "围场满族蒙古族自治县", "option": "B"}], "img": null, "no": 54}
-, {"answer": "A", "stem": "习近平总书记是在哪里提出了“绿水青山就是金山银山”的科学论断?", "options": [{"content": "浙江省安吉县", "option": "A"}, {"content": "福建省宁德市", "option": "B"}], "img": null, "no": 55}
-, {"answer": "A", "stem": "我国的主要航天中心有四川西昌、甘肃酒泉、山西太原和____。", "options": [{"content": "海南文昌", "option": "A"}, {"content": "海南海口", "option": "B"}], "img": null, "no": 56}
-, {"answer": "A", "stem": "以下水域属于我国内水的是____。", "options": [{"content": "渤海", "option": "A"}, {"content": "黄海", "option": "B"}], "img": null, "no": 57}
-, {"answer": "B", "stem": "曾经的中共中央所在地西柏坡位于河北省的哪个县?", "options": [{"content": "正定县", "option": "A"}, {"content": "平山县", "option": "B"}], "img": null, "no": 58}
-, {"answer": "B", "stem": "我国的大地原点位于陕西省的____境内。", "options": [{"content": "西安市", "option": "A"}, {"content": "咸阳市", "option": "B"}], "img": null, "no": 59}
-, {"answer": "A", "stem": "我国的四大高原中，哪座高原以喀斯特地貌著称?", "options": [{"content": "云贵高原", "option": "A"}, {"content": "青藏高原", "option": "B"}], "img": null, "no": 60}]
+let questions = [{"answer": "B", "stem": "What is the full name of YICMG? (Multiple Choice)", "options": [
+{"content": "Youth Innovation Competition on Lancang-Mekong Region's Governance and Development.", "option": "A"}, 
+{"content": "Youth Innovation Competition on Global Governance.", "option": "B"},
+{"content": "澜沧江—湄公河流域治理与发展青年创新设计大赛", "option": "C"},
+{"content": "全球治理创新设计大赛", "option": "D"}], "img": null, "no": 1}
+
+
+ , {"answer": "A", "stem": "How many countries does the Lancang-Mekong River flow through? (Single Choice)", "options": [
+{"content": "4", "option": "A"}, 
+{"content": "5", "option": "B"},
+{"content": "6", "option": "C"},
+{"content": "7", "option": "D"}], "img": null, "no": 2}
+
+
+ , {"answer": "B", "stem": "What are the purposes of YICMG? (Single Choice)", "options": [
+{"content": "To encourage youths from the six countries to pay attention to the real-world issues on Lancang-Mekong sub-regional governance and development, and to carry out in-depth field research, social practice and extracurriculum research.", "option": "A"}, 
+{"content": "To propose effective solutions to solve problems and promote development through team work, to improve learning outcomes and program quality through international competitions, and to implement the program to be innovation and entrepreneurship if conditions permit.", "option": "B"},
+{"content": "To encourage the high education and academic research institutions of the six countries to support the extracurricular research and entrepreneurial actions of the youth, and to form a network in order to provide a strong support for the prosperity of regional cultural communication, the enhancement of international cooperation, and the promotion of watershed governance model innovation.", "option": "C"},
+{"content": "To encourage the social forces of the six countries to jointly establish the Youth Innovation and Entrepreneurship Fund for the Governance and Development of the Lancang-Mekong Region, and to support regional cooperation and youth development.", "option": "D"},
+{"content": "All of the above.", "option": "E"}], "img": null, "no": 3}
+
+
+ , {"answer": "A", "stem": "Which is the alternative name of the Lancang-Mekong River? (Single Choice)", "options": [
+{"content": "The Oriental Nile.", "option": "A"}, 
+{"content": "The Oriental Congo River.", "option": "B"},
+{"content": "The Oriental Danube.", "option": "C"},
+{"content": "The Oriental Rhine.", "option": "D"},
+{"content": "The Oriental Amazon River.", "option": "E"},
+{"content": "All of the above.", "option": "F"}], "img": null, "no": 4}
+
+
+ , {"answer": "B", "stem": "Where does the Lancang-Mekong River originate? (Single Choice)", "options": [
+{"content": "In Xining, Qinghai.", "option": "A"}, 
+{"content": "In Yushu, Qinghai.", "option": "B"},
+{"content": "In Nanning, Guangxi.", "option": "C"},
+{"content": "In Lhasa, Tibet.", "option": "D"}], "img": null, "no": 5}
+
+
+ , {"answer": "A", "stem": "Along the Belt and Road, which is the port city that is located in the Lancang-Mekong basin? (Single Choice)", "options": [
+{"content": "Rotterdam", "option": "A"}, 
+{"content": "Murmansk", "option": "B"},
+{"content": "Hanoi", "option": "C"},
+{"content": "Madagascar", "option": "D"},
+{"content": "New York.", "option": "E"},
+{"content": "All of the above.", "option": "F"}], "img": null, "no": 6}
+
+
+ , {"answer": "A", "stem": "Which city does the YICMG International Organizing Committee locates in? (Single Choice)", "options": [
+{"content": "Hanoi, Vietnam.", "option": "A"}, 
+{"content": "Phnom Penh, Cambodia.", "option": "B"},
+{"content": "Bangkok, Thailand.", "option": "C"},
+{"content": "Shanghai, China.", "option": "D"}], "img": null, "no": 7}
+
+
+ , {"answer": "B", "stem": "Which cities have YICMG been held in? (Single Choice)", "options": [
+{"content": "Qinghai, China; Vientiane, Laos; Bangkok, Thailand; Hanoi, Vietnam.", "option": "A"}, 
+{"content": "Qinghai, China; Phnom Penh, Cambodia; Vientiane, Laos; Nanning, China.", "option": "B"},
+{"content": "Qinghai, China; Phnom Penh, Cambodia; Bangkok, Thailand; Hanoi, Vietnam.", "option": "C"},
+{"content": "Qinghai, China; Nanning, China; Vientiane, Laos; Shanghai, China.", "option": "D"}], "img": null, "no": 8}
+
+
+ , {"answer": "A", "stem": "Which Lancang-Mekong Youth Training Camp for Innovation and Entrepreneurship is the one held in Qinghai, China in July, 2019? (Single Choice)", "options": [
+{"content": "The First.", "option": "A"}, 
+{"content": "The Second.", "option": "B"},
+{"content": "The Third.", "option": "C"},
+{"content": "The Fourth.", "option": "D"}], "img": null, "no": 9}
+
+
+ , {"answer": "B", "stem": "Which is the theme of YICMG 2019? (Single Choice)", "options": [
+{"content": "Green Consumption, Production and Social Development.", "option": "A"}, 
+{"content": "Climate Change and Cooperation against Disaster.", "option": "B"},
+{"content": "Sustainable Development of Water and Water Environment.", "option": "C"},
+{"content": "Environmental Protection and Economic Development.", "option": "D"},
+{"content": "All of the above.", "option": "E"}], "img": null, "no": 10}]
 module.exports = questions
