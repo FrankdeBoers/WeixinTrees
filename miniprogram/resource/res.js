fd99e72e6da64de232cbf5200573b4941cbeed3a
Frank <279375981@qq.com>
2019-06-22 23:05:08 +0800

    [小程序]修改问题答案

--- miniprogram/resource/res.js
+++ miniprogram/resource/res.js
@@ -1,18 +1,19 @@
-let questions = [{"answer": "B", "stem": "What is the full name of YICMG? (Multiple Choice)", "options": [
+let questions = [{
+  "answer": "A", "stem": "What is the full name of YICMG? (Single Choice)", "options": [
 {"content": "Youth Innovation Competition on Lancang-Mekong Region's Governance and Development.", "option": "A"}, 
 {"content": "Youth Innovation Competition on Global Governance.", "option": "B"},
-{"content": "澜沧江—湄公河流域治理与发展青年创新设计大赛", "option": "C"},
+{"content": "澜沧江—湄公河流域治理与发展创新设计大赛", "option": "C"},
 {"content": "全球治理创新设计大赛", "option": "D"}], "img": null, "no": 1}
 
 
- , {"answer": "A", "stem": "How many countries does the Lancang-Mekong River flow through? (Single Choice)", "options": [
+ , {"answer": "C ", "stem": "How many countries does the Lancang-Mekong River flow through? (Single Choice)", "options": [
 {"content": "4", "option": "A"}, 
 {"content": "5", "option": "B"},
 {"content": "6", "option": "C"},
 {"content": "7", "option": "D"}], "img": null, "no": 2}
 
 
- , {"answer": "B", "stem": "What are the purposes of YICMG? (Single Choice)", "options": [
+ , {"answer": "E", "stem": "What are the purposes of YICMG? (Single Choice)", "options": [
 {"content": "To encourage youths from the six countries to pay attention to the real-world issues on Lancang-Mekong sub-regional governance and development, and to carry out in-depth field research, social practice and extracurriculum research.", "option": "A"}, 
 {"content": "To propose effective solutions to solve problems and promote development through team work, to improve learning outcomes and program quality through international competitions, and to implement the program to be innovation and entrepreneurship if conditions permit.", "option": "B"},
 {"content": "To encourage the high education and academic research institutions of the six countries to support the extracurricular research and entrepreneurial actions of the youth, and to form a network in order to provide a strong support for the prosperity of regional cultural communication, the enhancement of international cooperation, and the promotion of watershed governance model innovation.", "option": "C"},
@@ -20,7 +21,7 @@ let questions = [{"answer": "B", "stem": "What is the full name of YICMG? (Multi
 {"content": "All of the above.", "option": "E"}], "img": null, "no": 3}
 
 
- , {"answer": "A", "stem": "Which is the alternative name of the Lancang-Mekong River? (Single Choice)", "options": [
+ , {"answer": "C", "stem": "Which is the alternative name of the Lancang-Mekong River? (Single Choice)", "options": [
 {"content": "The Oriental Nile.", "option": "A"}, 
 {"content": "The Oriental Congo River.", "option": "B"},
 {"content": "The Oriental Danube.", "option": "C"},
@@ -36,7 +37,7 @@ let questions = [{"answer": "B", "stem": "What is the full name of YICMG? (Multi
 {"content": "In Lhasa, Tibet.", "option": "D"}], "img": null, "no": 5}
 
 
- , {"answer": "A", "stem": "Along the Belt and Road, which is the port city that is located in the Lancang-Mekong basin? (Single Choice)", "options": [
+ , {"answer": "C", "stem": "Along the Belt and Road, which is the port city that is located in the Lancang-Mekong basin? (Single Choice)", "options": [
 {"content": "Rotterdam", "option": "A"}, 
 {"content": "Murmansk", "option": "B"},
 {"content": "Hanoi", "option": "C"},
@@ -45,7 +46,7 @@ let questions = [{"answer": "B", "stem": "What is the full name of YICMG? (Multi
 {"content": "All of the above.", "option": "F"}], "img": null, "no": 6}
 
 
- , {"answer": "A", "stem": "Which city does the YICMG International Organizing Committee locates in? (Single Choice)", "options": [
+ , {"answer": "D", "stem": "Which city does the YICMG International Organizing Committee locates in? (Single Choice)", "options": [
 {"content": "Hanoi, Vietnam.", "option": "A"}, 
 {"content": "Phnom Penh, Cambodia.", "option": "B"},
 {"content": "Bangkok, Thailand.", "option": "C"},
@@ -59,14 +60,14 @@ let questions = [{"answer": "B", "stem": "What is the full name of YICMG? (Multi
 {"content": "Qinghai, China; Nanning, China; Vientiane, Laos; Shanghai, China.", "option": "D"}], "img": null, "no": 8}
 
 
- , {"answer": "A", "stem": "Which Lancang-Mekong Youth Training Camp for Innovation and Entrepreneurship is the one held in Qinghai, China in July, 2019? (Single Choice)", "options": [
+ , {"answer": "C", "stem": "Which Lancang-Mekong Youth Training Camp for Innovation and Entrepreneurship is the one held in Qinghai, China in July, 2019? (Single Choice)", "options": [
 {"content": "The First.", "option": "A"}, 
 {"content": "The Second.", "option": "B"},
 {"content": "The Third.", "option": "C"},
 {"content": "The Fourth.", "option": "D"}], "img": null, "no": 9}
 
 
- , {"answer": "B", "stem": "Which is the theme of YICMG 2019? (Single Choice)", "options": [
+ , {"answer": "A", "stem": "Which is the theme of YICMG 2019? (Single Choice)", "options": [
 {"content": "Green Consumption, Production and Social Development.", "option": "A"}, 
 {"content": "Climate Change and Cooperation against Disaster.", "option": "B"},
 {"content": "Sustainable Development of Water and Water Environment.", "option": "C"},
